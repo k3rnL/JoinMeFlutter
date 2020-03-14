3a3f8f4d0454b2d0d08451f10b27613ffcef0299
Daniel Erwan <daniel.erwan31@gmail.com>
2020-03-14 19:22:09 +0100

    Fix api calls

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,56 +7,81 @@ import 'package:join_me/models/user.dart';
 class ApiService {
   ApiService();
 
-  String url = 'https://join-me-api.herokuapp.com';
+  static const String API_URL = 'https://join-me-api.herokuapp.com';
 
-  Future<void> createParty(String name, String address) async {
+  static Future<String> createParty(String name, String address) async {
     final Party party = Party();
     party.name = name;
     party.address = address;
-    final http.Response response = await http.post(url + '/party/create', body: jsonEncode(party));
+    final http.Response response =
+        await http.post(API_URL + '/party/create', body: jsonEncode(party));
     if (response.statusCode == 200) {
-      print('createParty Response body: ${response.body}');
+      final Map<String, dynamic> json = jsonDecode(response.body);
+      return json['id'].toString();
     }
+    return '';
   }
 
-  Future<void> addUsersToParty(List<String> phones, int partyId) async {
-    final List<User> users = <User> [];
+  static Future<void> addUsersToParty(List<String> phones, int partyId) async {
+    final List<User> users = <User>[];
     for (String phone in phones) {
       final User user = User();
       user.phone = phone;
       users.add(user);
     }
-    final http.Response response = await http.patch(url + '/party/' + partyId.toString(), body: jsonEncode(users));
+    final http.Response response = await http.patch(
+        API_URL + '/party/' + partyId.toString(),
+        body: jsonEncode(users));
     if (response.statusCode == 200) {
       print('addUsersToParty Response body: ${response.body}');
     }
   }
 
-  Future<void> registerUser(String uid, String fcmToken) async {
-    final http.Response response = await http.post(url + '/users/' + uid + '/update_token',  body: <String, dynamic> {'fcm_token': fcmToken});
+  static Future<void> addUsersToPartyByUid(List<String> uids, String partyId) async {
+    final List<User> users = uids.map<User>((String uid) {
+      final User user = User();
+      user.uid = uid;
+      return user;
+    }).toList();
+
+    final http.Response response = await http.patch(
+        API_URL + '/party/' + partyId.toString(),
+        body: jsonEncode(users));
+    if (response.statusCode == 200) {
+      print('addUsersToParty Response body: ${response.body}');
+    }
+  }
+
+  static Future<void> registerUser(String uid, String fcmToken) async {
+    final http.Response response = await http.post(
+        API_URL + '/users/' + uid + '/update_token',
+        body: <String, dynamic>{'fcm_token': fcmToken});
     if (response.statusCode == 200) {
       print('registerUser Response body: ${response.body}');
     }
   }
 
-  Future<void> updateUserToken(String uid, String fcmToken) async {
-    final http.Response response = await http.patch(url + '/users/' + uid + '/update_token', body: <String, dynamic> {'fcm_token': fcmToken});
+  static Future<void> updateUserToken(String uid, String fcmToken) async {
+    final http.Response response = await http.patch(
+        API_URL + '/users/' + uid + '/update_token',
+        body: <String, dynamic>{'fcm_token': fcmToken});
     if (response.statusCode == 200) {
       print('updateUserToken Response body: ${response.body}');
     }
   }
 
-  Future<User> getUser(String uid) async {
-    final http.Response response = await http.get(url + '/users/' + uid);
+  static Future<User> getUser(String uid) async {
+    final http.Response response = await http.get(API_URL + '/users/' + uid);
     if (response.statusCode == 200) {
       print('getUser Response body: ${response.body}');
-      return jsonDecode(response.body);
+      return User.fromJson(response.body);
     }
     return null;
   }
 
-  Future<Party> getParty(int id) async {
-    final http.Response response = await http.get(url + '/party/' + id.toString());
+  static Future<Party> getParty(int id) async {
+    final http.Response response =
+        await http.get(API_URL + '/party/' + id.toString());
     if (response.statusCode == 200) {
       print('getParty Response body: ${response.body}');
       return jsonDecode(response.body);
@@ -64,8 +89,8 @@ class ApiService {
     return null;
   }
 
-  Future<void> unsubscribeToParty(String uid, int partyId) async {
-    final http.Response response = await http.get(url + '/users/' + uid);
+  static Future<void> unsubscribeToParty(String uid, int partyId) async {
+    final http.Response response = await http.get(API_URL + '/users/' + uid);
     if (response.statusCode == 200) {
       print('unsubscribeToParty Response body: ${response.body}');
     }
