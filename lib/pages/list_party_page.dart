f1c5a4e72c640392eb232819a3003195583fd6a9
cerea <adrien.cere@itine.run>
2020-03-21 02:40:21 +0100

    Begin

--- lib/pages/list_party_page.dart
+++ lib/pages/list_party_page.dart
@@ -4,6 +4,7 @@ import 'package:join_me/models/user.dart';
 import 'package:join_me/services/api_service.dart';
 import 'package:provider/provider.dart';
 import 'package:join_me/components/list_item.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 void main() => runApp(ListPartyPage());
 
@@ -18,8 +19,7 @@ class ListPartyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder<User>(
-        future:
-            ApiService.getUser(Provider.of<User>(context, listen: false).uid),
+        future: ApiService.getUser(Provider.of<User>(context, listen: false).uid),
         builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
           if (snapshot.hasData) {
             final User user = snapshot.data;
@@ -27,47 +27,35 @@ class ListPartyPage extends StatelessWidget {
             return ListView.builder(
                 itemCount: user.invitations.length,
                 itemBuilder: (BuildContext ctxt, int index) {
-                  return Dismissible(
-                      key: Key(user.invitations[index]),
-                      onDismissed: (DismissDirection direction) {
-                        user.invitations.removeAt(index);
-                        Scaffold.of(context).showSnackBar(const SnackBar(
-                          content: Text('item supprimé'),
-                        ));
-                      },
-                      child: FutureBuilder<Party>(
-                        future: ApiService.getParty(user.invitations[index]),
-                        builder: (BuildContext context,
-                            AsyncSnapshot<Party> snapshot) {
-                          if (snapshot.hasData) {
-                            final Party party = snapshot.data;
-                            final String nbMembersString =
-                                party.members.length.toString() + ' members';
-                            final String encoded =
-                                Uri.encodeFull(party.address);
+                  return FutureBuilder<Party>(
+                    future: ApiService.getParty(user.invitations[index]),
+                    builder: (BuildContext context, AsyncSnapshot<Party> snapshot) {
+                      if (snapshot.hasData) {
+                        final Party party = snapshot.data;
+                        final String nbMembersString = party.members.length.toString() + ' members';
+                        final String encoded = Uri.encodeFull(party.address);
 
-                            return Column(
-                              children: <Widget>[
-                                ListItem(
-                                  image: NetworkImage(
-                                      'https://maps.googleapis.com/maps/api/staticmap?center=$encoded&zoom=13&size=1800x1800&maptype=roadmap&markers=color:blue%7C$encoded&key=AIzaSyAfv8IPCxhiURtrI8tDyQptGEVQoOl0G3c'),
-                                  title: party.name,
-                                  subtitle: nbMembersString,
-                                  onTap: () {
-                                    print('Mettre lappel pour party Detail');
-                                  },
-                                ),
-                              ],
-                            );
-                          } else {
-                            if (snapshot.hasError) {
-                              print(snapshot.error);
-                            }
-                            return const Text('');
-                          }
-                        },
-                      ));
-                });
+                        return Column(
+                          children: <Widget>[
+                            ListItem(
+                              image: NetworkImage('https://maps.googleapis.com/maps/api/staticmap?center=$encoded&zoom=13&size=1800x1800&maptype=roadmap&markers=color:blue%7C$encoded&key=AIzaSyAfv8IPCxhiURtrI8tDyQptGEVQoOl0G3c'),
+                              title: party.name,
+                              subtitle: nbMembersString,
+                              onTap: () {
+                                print('Mettre lappel pour party Detail');
+                              },
+                            ),
+                          ],
+                        );
+                      } else {
+                        if (snapshot.hasError)
+                          print(snapshot.error);
+                        return const Text('');
+                      }
+                    },
+                  );
+                }
+            );
           } else {
             if (snapshot.hasError) {
               print(snapshot.error);
